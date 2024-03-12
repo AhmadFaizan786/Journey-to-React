3dd99981e402eebaf1e324709286feddfe069cb0
Faizan Ahmad <fa371250@gmail.com>
2024-03-12 16:30:58 +0530

    second commit

--- myapp/myfirstviteproject/src/App.jsx
+++ myapp/myfirstviteproject/src/App.jsx
@@ -1,7 +1,12 @@
+import File1 from "./File1"
 
 function App() {
   return (
-      <h1>Hello Vite+React | Faizan Speaking...</h1>
+    <>
+     <h1>Hello Vite+React | Faizan Speaking...</h1>
+     <h2>Welcome to the world of react..</h2>
+     <File1/>
+    </>
   )
 }
 
---
+++ myapp/myfirstviteproject/src/File1.jsx
@@ -0,0 +1,7 @@
+function Hello(){
+    return(
+        <h2>Entering the world of react </h2>
+    )
+}
+
+export default Hello
