a73f4d42be4677ac042352c0262d6f466a95495e
Faizan Ahmad <fa371250@gmail.com>
2024-03-13 16:53:28 +0530

    fourth commit

---
+++ Sheriyans/vite-project/src/Components/Header.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+        <div className="mynav">
+        <div className="nav-heading">
+          <h2>Faizan Ahmad</h2>
+        </div>
+        <div className="nav-links">
+          <a>
+            <h3>About</h3>
+          </a>
+          <a>
+            <h3>Projects</h3>
+          </a>
+          <a>
+            <h3>Skills</h3>
+          </a>
+          <a>
+            <h3>Certifications</h3>
+          </a>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Header
---
+++ Sheriyans/vite-project/src/Components/Hero.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+
+const Hero = () => {
+    
+    const [first, setfirst] = useState(80)
+  return (
+    <>
+    <div className='hero-button'>
+    <button >Click</button>
+    </div>
+    </>
+  )
+}
+
+export default Hero
