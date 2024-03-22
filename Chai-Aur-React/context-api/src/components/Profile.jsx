86fda0b9bb206bd5589661c3ebe02411f973b630
Faizan Ahmad <fa371250@gmail.com>
2024-03-22 09:31:21 +0530

    context-api

---
+++ Chai-Aur-React/context-api/src/components/Profile.jsx
@@ -0,0 +1,21 @@
+import React, {useContext} from 'react'
+import UserContext from '../Context/UserContext'
+
+
+function Profile() {
+
+  const {user} = useContext(UserContext)
+
+  // if(!user) return <div>please login</div>
+
+  // return <div>welcome {user.username}</div>
+
+  if (!user) {
+    return <div>please login</div>
+    
+  } else {
+    return <div>welcome {user.username}</div>
+  }
+}
+
+export default Profile
